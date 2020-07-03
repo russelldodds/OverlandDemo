ccbb72f550b2a523d5da1a8d6bccbfab59c38ef8
Rusell Dodds <russell.dodds@tangocard.com>
2020-07-02 18:20:37 -0700

    import

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // https://www.youtube.com/watch?v=mbzXIOKZurA
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
